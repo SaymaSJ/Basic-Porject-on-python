3e88e07ff8adba1ccb4d081d00a40a6872e6edbe
Sayma Sultana Jhara <saymamte@gmail.com>
2024-02-10 13:03:45 +0600

    Update Project1.py

--- Project1.py
+++ Project1.py
@@ -1,4 +1,28 @@
 print("hello world")
-a=2
+a=10
 b=2
 print(a+b)
+print(a/b)
+print(a//b)
+print(a**b)
+print(a*b)
+
+
+
+print("Both conditions are true")
+
+input("Please enter a:")
+input("Please enter b:")
+
+
+# a=int(input("Enter the value for a"))
+# b=int(input("Enter the value for b"))
+# print(a+b)
+# print(a/b)
+# print(a//b)
+# print(a**b)
+# print(a*b)
+
+
+
+# print("Both conditions are true")
