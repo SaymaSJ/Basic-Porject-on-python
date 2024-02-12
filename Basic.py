ff19e934227c2c3cc4085b38bf772cc684e29dcd
Sayma Sultana Jhara <saymamte@gmail.com>
2024-02-12 23:24:15 +0600

    Added

---
+++ Basic.py
@@ -0,0 +1,59 @@
+print("hello world")
+a=10
+b=2
+print(a+b)
+print(a/b)
+print(a//b)
+print(a**b)
+print(a*b)
+
+
+#Variables
+x=int(5)
+y=str(5.58)
+z=float(5.594)
+
+print(x)
+print(y)
+print(z)
+x=x+1
+print(type(x))
+print(type(y))
+
+# x,y,z= "Orange","Apple", "Banana" #Unpacking
+# print(x)
+# print(y)
+# print(x)
+
+fruits=["Orange","Apple", "Banana"]
+x,y,z=fruits
+print(x)
+print(y)
+print(x)
+print(fruits[0])
+print(fruits[1])
+
+# print("Both conditions are true")
+
+# input("Please enter a:")
+# input("Please enter b:")
+
+# a=int(input("Enter the value for a:"))
+# b=int(input("Enter the value for b:"))
+# add=a+b
+# sub=a-b
+# while True:
+#     print("Please + for add: ")
+#     print("Please - for sub: ")
+# user= input( " :")
+# if user in ("add") or user =="+" :
+#  print(add)
+# print(a/b)
+
+# print(a//b)
+# print(a**b)
+# print(a*b)
+
+
+
+# print("Both conditions are true")
